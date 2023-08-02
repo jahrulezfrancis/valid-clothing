6f667577fee84e5b0097537f97255d1d5aa0f0ff
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-02 19:11:11 +0100

    Built directory page and home component

---
+++ src/Components/Directory/Directory.component.jsx
@@ -0,0 +1,11 @@
+import CategoryItem from "../Category-item/category-item.component";
+import { CategoryInfo } from "../Category-item/category-info";
+
+const Directory = () => {
+    return (
+        <CategoryItem categoryList={CategoryInfo} />
+
+    )
+}
+
+export default Directory;

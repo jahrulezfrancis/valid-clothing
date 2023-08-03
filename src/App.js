c288f74fce3a411f196b24bf799a22f04c23033b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-03 02:06:52 +0100

    Adding navigation links and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./Components/routes/Home/Home.component";
+import NavBar from './Components/routes/Navigation/navigation-bar.component';
+import Signin from './Components/routes/Sign-in/sign-in.component';
+
+
+const Shop = () => {
+  return (
+    <h2>Shop page</h2>
+  )
+}
 
 function App() {
   return (
     <div className="app">
+      <Routes>
+        <Route path='/' element={<NavBar />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="sign-in" element={<Signin />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/routes/Navigation/navigation-bar.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import "./navigation-bar.styles.css"
+import { ReactComponent as CrownLogo } from "../../../Assets/crown.svg"
+
+
+const NavBar = () => {
+    return (
+        <Fragment>
+            <div className="nav-bar-container">
+                <Link className="nav-logo-container" to='/'>
+                    <div>
+                        <CrownLogo />
+                    </div>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to="shop">Shop</Link>
+                    <Link className="nav-link" to="shop">Contact</Link>
+                    <Link className="nav-link" to="sign-in">Sign In</Link>
+                    <Link className="nav-link" to="shop">Cart</Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default NavBar;

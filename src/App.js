da93709298bbae2e9835ac1297df2a37a67b2411
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-08 22:53:39 +0100

    Updated path to authentication page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from "./Components/routes/Home/Home.component";
 import NavBar from './Components/routes/Navigation/navigation-bar.component';
-import Signin from './Components/routes/Sign-in/sign-in.component';
-
+import Authentication from './Components/routes/Authentication/authentication.component';
 
 const Shop = () => {
   return (
@@ -18,7 +17,7 @@ function App() {
         <Route path='/' element={<NavBar />}>
           <Route index element={<Home />} />
           <Route path="shop" element={<Shop />} />
-          <Route path="sign-in" element={<Signin />} />
+          <Route path="auth" element={<Authentication />} />
         </Route>
       </Routes>
     </div>

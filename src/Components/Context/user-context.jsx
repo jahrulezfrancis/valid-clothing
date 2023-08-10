2aa3d5f1f8d3067004e0653d709a09650f8a29d3
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-10 09:56:23 +0100

    Created react-context component and wrap it around the parent component

---
+++ src/Components/Context/user-context.jsx
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+import { onAuthStateChangedListener } from "../Utils/Firebase/firebase-.utils";
+
+
+export const UserContext = createContext({
+    currentUser: null,
+    setCurrentUser: () => null
+})
+
+export const UserProvider = ({ children }) => {
+
+    const [currentUser, setCurrentUser] = useState(null)
+    const value = { currentUser, setCurrentUser };
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChangedListener((user) => {
+            console.log(user);
+        })
+        return unSubscribe;
+    }, [])
+
+    return (
+        <UserContext.Provider value={value}>
+            {children}
+        </UserContext.Provider>
+    )
+}

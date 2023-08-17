c549fe53348877621bc6f70aaa2f04ab22cf05ec
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-17 23:59:47 +0100

    Updated title to switch route dynamically

--- src/Components/Catogory-preview/category-preview.component.jsx
+++ src/Components/Catogory-preview/category-preview.component.jsx
@@ -1,11 +1,12 @@
 import "./category-preview.style.scss";
 import ProductsCard from "../Product-Card/product-card.compnonent";
+import { Link } from "react-router-dom";
 
 const CategoryPreview = ({ title, products }) => {
     return (
         <div className="category-preview-container">
             <h2>
-                <span className="title">{title.toUpperCase()}</span>
+                <Link to={title} className="title">{title.toUpperCase()}</Link>
             </h2>
             <div className="preview">
                 {products.filter((_, idx) => idx < 4).map((product) => <ProductsCard products={product} />)}

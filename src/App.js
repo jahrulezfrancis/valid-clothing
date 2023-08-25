bf2da21c0d242028b4b3a4fd0898088563cfe7ce
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-25 16:58:36 +0100

    Migrated userAuthlistener from user context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from "react";
+import { createUserDocumentFromAuth, onAuthStateChangedListener } from "./Components/Utils/Firebase/firebase.utils";
+import { setCurrentUser } from './Components/Store/user/user.action';
+
 import Home from "./Components/routes/Home/Home.component";
 import NavBar from './Components/routes/Navigation/navigation-bar.component';
 import Authentication from './Components/routes/Authentication/authentication.component';
 import ShopPage from './Components/routes/Shop/shop.component';
 import CheckoutPage from './Components/routes/Checkout/checkout.component';
+import { useDispatch } from 'react-redux';
 
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const unSubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user)
+      }
+      dispatch(setCurrentUser(user))
+    })
+    return unSubscribe;
+  }, [dispatch])
+
   return (
     <div className="app">
       <Routes>

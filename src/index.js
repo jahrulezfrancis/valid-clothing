e5065bb87257be482dedec6359d01a9fc9c17d29
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-25 16:52:48 +0100

    Added redux/react-redux/redux-toolkit packages and configured global store provdier

--- src/index.js
+++ src/index.js
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './Components/Context/user-context';
 import { CategoryProvider } from './Components/Context/category-context';
 import { CartProvider } from './Components/Context/cart-context';
+import { Provider } from 'react-redux';
+import { store } from './Components/Store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoryProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoryProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );

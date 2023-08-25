eddbcdccbe433fbd959cbc36aa359ab3e8feca40
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-25 17:00:35 +0100

    Replaced usecontext with useSelectors and changed logout method

--- src/Components/routes/Navigation/navigation-bar.component.jsx
+++ src/Components/routes/Navigation/navigation-bar.component.jsx
@@ -1,22 +1,18 @@
 import { Fragment, useContext } from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { NavContainer, LogoContainer, LinksContainer, Navlink } from "./navigation-bar.styles"
-// import { ReactComponent as CrownLogo } from "../../../Assets/crown.svg"
 import { ReactComponent as ValidLogo } from "../../../Assets/Shopping-u-draw.svg"
-import { UserContext } from "../../Context/user-context";
-import { signOutUser } from "../../Utils/Firebase/firebase-.utils";
+import { signOutUser } from "../../Utils/Firebase/firebase.utils";
 import CartIcon from "../../Cart-Icon/cart-icon.component";
 import CartDropDown from "../../Cart-dropDown/cart-drop-down.component";
 import { CartContext } from "../../Context/cart-context";
+import { selectCurrentUser } from "../../Store/user/userSelector";
 
 
 const NavBar = () => {
-    const { currentUser, setCurrentUser } = useContext(UserContext);
+    const currentUser = useSelector(selectCurrentUser);
     const { isCartOpen } = useContext(CartContext)
-    const handleLogout = async () => {
-        await signOutUser()
-        setCurrentUser(null)
-    }
 
     return (
         <Fragment>
@@ -29,7 +25,7 @@ const NavBar = () => {
                 <LinksContainer>
                     <Navlink to="shop">Shop</Navlink>
                     {currentUser ? (
-                        <Navlink onClick={handleLogout}>Sign out</Navlink>
+                        <Navlink onClick={signOutUser}>Sign out</Navlink>
                     ) : (
                         <Navlink to="auth">Sign in</Navlink>
                     )}

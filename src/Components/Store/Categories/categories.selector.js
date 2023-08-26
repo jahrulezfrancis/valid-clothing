76fcdee62e241ae34993c6362e5f93084dcf050e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-26 22:05:56 +0100

    Storing categories docs to categories selector

--- src/Components/Store/Categories/categories.selector.js
+++ src/Components/Store/Categories/categories.selector.js
@@ -1,2 +1,15 @@
+import { createSelector } from "reselect";
 
-export const CategoriesMapSelector = (state) => state.categories.categoriesMap;
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector([selectCategoryReducer], (categoriesSlice) => categoriesSlice.categories)
+
+export const CategoriesSelector = createSelector(
+    [selectCategories],
+    (categories) =>
+        categories.reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {})
+)

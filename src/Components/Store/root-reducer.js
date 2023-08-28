4fa2df332a3d782490c43fa47ddb05ce6d31f5aa
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-28 21:56:29 +0100

    Removed cart reducer import

--- src/Components/Store/root-reducer.js
+++ src/Components/Store/root-reducer.js
@@ -1,10 +1,9 @@
 import { combineReducers } from "redux";
 import { UserReducer } from "./user/user-reducer";
 import { CategoriesReducer } from "./Categories/categories.reducer";
-import { CartReducer } from "./Cart/cart.reducer";
+
 
 export const RootReducer = combineReducers({
     user: UserReducer,
     categories: CategoriesReducer,
-    cart: CartReducer
 })

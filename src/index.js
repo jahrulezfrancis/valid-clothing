f3386069aa534507c47a88f3a6151d1c9b47d125
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-29 17:20:58 +0100

    Removed persist storage provider

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from "redux-persist/integration/react"
 import App from './App';
 import './index.scss';
 import { CartProvider } from './Components/Context/cart-context';
-import { persistor, store } from './Components/Store/store';
+import { store } from './Components/Store/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <CartProvider>
             <App />
           </CartProvider>
         </BrowserRouter>
-      </PersistGate>
     </Provider>
   </React.StrictMode>
 );

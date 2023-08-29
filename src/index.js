fade83f04e38da4b39a80a56600a157cf3286ece
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-29 17:33:43 +0100

    Deleted cartContext component and removed context provider from index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.scss';
-import { CartProvider } from './Components/Context/cart-context';
 import { store } from './Components/Store/store';
 
 
@@ -13,9 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
         <BrowserRouter>
-          <CartProvider>
             <App />
-          </CartProvider>
         </BrowserRouter>
     </Provider>
   </React.StrictMode>

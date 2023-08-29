279dec0f682cb4656f91de245f4d2b5ef9fff92d
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-29 01:19:03 +0100

    Wrapped app with redux-persist gate provider

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './Components/Context/cart-context';
 import { Provider } from 'react-redux';
-import { store } from './Components/Store/store';
+import { PersistGate } from "redux-persist/integration/react"
+import App from './App';
+import './index.scss';
+import { CartProvider } from './Components/Context/cart-context';
+import { persistor, store } from './Components/Store/store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
           <CartProvider>
             <App />
           </CartProvider>
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );

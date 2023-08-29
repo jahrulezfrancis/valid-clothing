e1da75ab16f69e57dd40f55fd9d523c7e9a45367
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-29 01:20:36 +0100

    Updating middleWares

--- src/Components/Store/store.js
+++ src/Components/Store/store.js
@@ -1,10 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { RootReducer } from "./root-reducer";
-import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { loggerMiddleware } from "./MiddleWares/logger";
 
-const middlewares = [logger]; // Include any other middlewares here
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const persitedRedecuer = persistReducer(persistConfig, RootReducer)
+
+const middlewares = [loggerMiddleware];
+
 
 export const store = configureStore({
-  reducer: RootReducer, // Specify the root reducer
-  middleware: middlewares, // Specify middleware
+  reducer: persitedRedecuer,
+  middleware: middlewares,
 });
+
+export const persistor = persistStore(store);

e5ffb21fccd5e194bad6f375d2ce477117a8f4e2
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-30 22:55:29 +0100

    Created serverless function to handle stripe payment

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.scss';
 import { store } from './Components/Store/store';
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './Components/Utils/Stripe/Stripe.utils';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+      <BrowserRouter>
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );

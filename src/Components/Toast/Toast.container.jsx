f356803a671af72c4a48e34253652163d5e65138
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-30 23:01:26 +0100

    Created custom reuseable toast component

---
+++ src/Components/Toast/Toast.container.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const showToast = (message, type) => {
+  switch (type) {
+    case 'info':
+      toast.info(message);
+      break;
+    case 'success':
+      toast.success(message);
+      break;
+    case 'warning':
+      toast.warning(message);
+      break;
+    case 'error':
+      toast.error(message);
+      break;
+    default:
+      toast(message);
+  }
+};
+
+const Toast = () => {
+  return (
+    <div>
+      <ToastContainer autoClose={4000} hideProgressBar />
+    </div>
+  );
+};
+
+export { showToast, Toast };

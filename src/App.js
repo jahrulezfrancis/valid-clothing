c18fb16336828d678db40aa3f12d49b5187180be
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-10-12 00:45:53 +0100

    Implementing auth check to match route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import { useEffect } from "react";
 import { createUserDocumentFromAuth, fetchTransactionHistory, onAuthStateChangedListener } from "./Components/Utils/Firebase/firebase.utils";
-import { setCurrentUser } from './Components/Store/user/user-slice';
-import ProfilePage from './Components/routes/Profile/Profile';
+import { setIsAuthenticated, setCurrentUser } from './Components/Store/user/user-slice';
+import ProfilePage from './Components/routes/Profile/Profile.Component';
 import Home from "./Components/routes/Home/Home.component";
 import NavBar from './Components/routes/Navigation/navigation-bar.component';
 import Authentication from './Components/routes/Authentication/authentication.component';
@@ -10,21 +10,23 @@ import ShopPage from './Components/routes/Shop/shop.component';
 import CheckoutPage from './Components/routes/Checkout/checkout.component';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectCurrentUser } from './Components/Store/user/userSelector';
+import { selectAuth } from './Components/Store/user/authStatusSelector';
 
 
 function App() {
   const dispatch = useDispatch()
   const currentUser = useSelector(selectCurrentUser)
+  const isAuthenticated = useSelector(selectAuth)
   const uID = currentUser ? currentUser.uid : ''
 
   useEffect(() => {
     fetchTransactionHistory(uID)
-    .then((transactions) => {
-      console.log(transactions)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+      .then((transactions) => {
+        console.log(transactions)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }, [uID])
 
 
@@ -33,8 +35,9 @@ function App() {
       if (user) {
         createUserDocumentFromAuth(user)
       }
+      dispatch(setIsAuthenticated(true))
       dispatch(setCurrentUser(user))
-    })    
+    })
     return unSubscribe;
   }, [dispatch])
 
@@ -44,8 +47,8 @@ function App() {
         <Route path='/' element={<NavBar />}>
           <Route index element={<Home />} />
           <Route path="shop/*" element={<ShopPage />} />
-          <Route path="profile" element={<ProfilePage />} />
-          <Route path="auth" element={<Authentication />} />
+          <Route path="profile" element={!isAuthenticated ? <Authentication /> : <ProfilePage />} />
+          <Route path={!isAuthenticated ? "auth" : "shop/*"} element={!isAuthenticated ? <Authentication /> : <ShopPage />} />
           <Route path="checkout" element={<CheckoutPage />} />
         </Route>
       </Routes>

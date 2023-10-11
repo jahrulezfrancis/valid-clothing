e2c040026f90be78e85e7db660941c98f3333765
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-10-12 00:48:19 +0100

    Added authenticated state

--- src/Components/Store/user/user-slice.js
+++ src/Components/Store/user/user-slice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const InitialState = {
-    currentUser: null
+    currentUser: null,
+    isAuthenticated: false
 }
 
 export const userSlice = createSlice({
@@ -10,11 +11,14 @@ export const userSlice = createSlice({
     reducers: {
         setCurrentUser(state, action) {
             state.currentUser = action.payload;
+        },
+        setIsAuthenticated(state, action) {
+            state.isAuthenticated = action.payload
         }
     }
 })
 
-export const { setCurrentUser } = userSlice.actions;
+export const { setCurrentUser, setIsAuthenticated } = userSlice.actions;
 
 export const userReducer = userSlice.reducer;
 

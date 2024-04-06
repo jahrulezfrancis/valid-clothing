c737bbd0731e830782948247ed83c65acf94fe7e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-06 22:41:39 +0100

    Checking provided information for profile update

--- src/Components/routes/Profile/Profile.Component.jsx
+++ src/Components/routes/Profile/Profile.Component.jsx
@@ -17,7 +17,7 @@ export default function ProfilePage() {
   const currentUser = useSelector(selectCurrentUser);
   const [loadUser, setLoadUser] = useState(false);
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     setTimeout(() => {
       setLoadUser(true);
@@ -30,38 +30,38 @@ export default function ProfilePage() {
   }
 
   return (
-    currentUser ? 
-    <ProfileContainer>
-      {loadUser && (
-        <>
-          <PictureSection>
-            <div
-              style={{ display: "flex", flexDirection: "column", width: "50%" }}
-            >
-              <ProfileImage
-                src={currentUser.photoURL}
-                alt={currentUser.displayName}
+    currentUser ?
+      <ProfileContainer>
+        {loadUser && (
+          <>
+            <PictureSection>
+              <div
+                style={{ display: "flex", flexDirection: "column", width: "50%" }}
+              >
+                <ProfileImage
+                  src={currentUser.photoURL}
+                  alt={currentUser.displayName}
+                />
+                <ClassicHeading>{currentUser.displayName}</ClassicHeading>
+              </div>
+              <FaPowerOff
+                style={{ cursor: "pointer" }}
+                onClick={handleLogout}
+                fontSize={30}
+                color="red"
               />
-              <ClassicHeading>{currentUser.displayName}</ClassicHeading>
-            </div>
-            <FaPowerOff
-              style={{ cursor: "pointer" }}
-              onClick={handleLogout}
-              fontSize={30}
-              color="red"
-            />
-          </PictureSection>
-          <hr />
-          <ProfileBody>
-            <h5>Name: {currentUser.displayName}</h5>
-            <h5>Email: {currentUser.email}</h5>
-            <h5>Phone Number: {currentUser.phoneNumber}</h5>
-          </ProfileBody>
-          <h2>Your Purchase history</h2>
-          <TransactionHistoryTable />
-        </>
-      )}
-    </ProfileContainer> :
-    <Navigate to="/auth" replace="true"/>
+            </PictureSection>
+            <hr />
+            <ProfileBody>
+              <h5>Name: {currentUser.displayName !== null ? currentUser.displayName : "Not provided, kindly update your profile"}</h5>
+              <h5>Email: {currentUser.email}</h5>
+              <h5>Phone Number: {currentUser.phoneNumber !== null ? currentUser.phoneNumber : "Not provided, kindly update your profile"}</h5>
+            </ProfileBody>
+            <h2>Your Purchase history</h2>
+            <TransactionHistoryTable />
+          </>
+        )}
+      </ProfileContainer> :
+      <Navigate to="/auth" replace="true" />
   );
 }
